10b88d18efb9e88107898c43deb56f5d2da5db4d
DoanCongQui <congt4644@gmail.com>
2025-02-16 02:28:22 +0700

    Update update lib

--- lib.py
+++ lib.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-class active_func():
+
+class active_func:
     def __init__(self) -> None:
         pass
 
@@ -9,36 +10,36 @@ class active_func():
             return 1
         else:
             return 0
-        
+
     def _sign(self, net):
         if net >= 0:
             return 1
         else:
             return -1
-        
+
     def _linear(self, net):
         return net
-    
+
     def _ReLU(self, net):
         if net >= 0:
             return net
         else:
             return 0
-    
+
     def _sigmoid(self, net):
-        return 1/(1+np.exp(-net))
-    
+        return 1 / (1 + np.exp(-net))
+
     def _tanh(self, net):
-        return (1-np.exp(-2*net))/(1+np.exp(-2*net))
+        return (1 - np.exp(-2 * net)) / (1 + np.exp(-2 * net))
+
 
 class Perceptron:
     def __init__(self):
         self.active = active_func()
 
-    def tranning(self, X, W, max_epoch):
+    def tranning(self, X, W, max_epoch=1):
         pass
-        
+
 
 if __name__ == "__main__":
     print("Pass")
-
