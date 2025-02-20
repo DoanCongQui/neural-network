472bcd73704c702d386b221ab4ffd331c0eaeb82
DoanCongQui <congt4644@gmail.com>
2025-02-20 18:46:40 +0700

    Update nn.Perceptron

--- lib.py
+++ lib.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cv2
 
 
 class active_func:
@@ -34,13 +33,39 @@ class active_func:
         return (1 - np.exp(-2 * net)) / (1 + np.exp(-2 * net))
 
 
-class Perceptron:
-    def __init__(self):
+class NeuralNertwork:
+    def __init__(self, X, W, d, teta, max_epoch):
         self.active = active_func()
+        self.X = X
+        self.W = W
+        self.d = d
+        self.teta = teta
+        self.max_epoch = max_epoch
 
-    def tranning(self, X, W, max_epoch=1):
-        pass
+    def Perceptron(self):
+        epoch = 0
+        size = len(self.X) + 1
+        print(size)
+        E = 0
+        while epoch < self.max_epoch:
+            for i in range(size):
+                net = self.W.T @ self.X[:, i]
+                print(net)
+                y = self.active._step(net)
+                self.W += self.teta * (self.d[i] - y) * self.X[:, i]
+                E = E + 0.5 * (self.d[i] - y) ** 2
+                print(self.W)
+                print(E)
+
+            epoch += 1
+            if E == 0:
+                break
 
 
 if __name__ == "__main__":
-    print("Pass")
+    X = np.array([[1, 1, 1, 1], [0, 0, 1, 1], [0, 1, 0, 1]])
+    W = np.array([0.1, 0.3, 0.5])
+    d = np.array([0, 0, 0, 1])
+
+    nn = NeuralNertwork(X, W, d, 0.1, 1)
+    nn.Perceptron()
